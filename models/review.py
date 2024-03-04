c4455d46713c54746867bc6071ec3af9d5fc378b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-03-05 01:33:32 +0200

    upload: review.py

---
+++ models/review.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+from .base_model import BaseModel
+
+
+class Review(BaseModel):
+    place_id = ""  # it will be the Place.id later
+    user_id = ""  # it will be the User.id later
+    text = ""
