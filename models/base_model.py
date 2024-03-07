09309fecb6d6173e69478a1b85f58aca03678101
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-03-07 03:58:00 +0200

    code refactor and fix issue in to_dict()

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -36,7 +36,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -53,17 +53,17 @@ class BaseModel:
         the BaseModel instance to the storage.
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
         Returns a dictionary representation of the BaseModel instance.
 
         Returns:
-        -   data: A dictionary representation of the model instance.
+        -   _dict: A dictionary representation of the model instance.
         """
-        data = self.__dict__
-        data["__class__"] = self.__class__.__name__
-        data['created_at'] = data['created_at'].isoformat()
-        data['updated_at'] = data['updated_at'].isoformat()
-        return data
+        _dict = self.__dict__.copy()
+        _dict["__class__"] = self.__class__.__name__
+        _dict['created_at'] = _dict['created_at'].isoformat()
+        _dict['updated_at'] = _dict['updated_at'].isoformat()
+        return _dict
