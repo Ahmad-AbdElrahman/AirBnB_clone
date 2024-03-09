510308a11f1390ef2dc208610787c511ed510a66
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-03-09 16:43:04 +0200

    add import for classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the storage object of the engine module"""
 from models.engine.file_storage import FileStorage
+from models.engine.file_storage import classes
 
 storage = FileStorage()
 storage.reload()
