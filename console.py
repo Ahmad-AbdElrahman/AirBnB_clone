5fdda8c335bd53b7b87baa8b60bb3dbff83af789
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-03-10 01:41:28 +0200

    console

--- console.py
+++ console.py
@@ -333,6 +333,10 @@ class HBNBConsole(cmd.Cmd):
         Quit command to exit the program.
         """
         return True
+    
+    def help_quit(self):
+        """Help message for the quit command."""
+        print("Quit command to exit the program\n")
 
     def emptyline(self):
         """
